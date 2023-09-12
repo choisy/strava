5c44202415228688cfccc24f7c5d44b7df9e6ac3
Marc Choisy <mchoisy@oucru.org>
2023-09-12 12:36:02 +0700

    Starting the code file

---
+++ code.R
@@ -0,0 +1,7 @@
+library(rStrava)
+
+athl_fun(34225766, trace = FALSE)
+
+install.packages("~/Desktop/FITfileR-master", repos = NULL, type = "source")
+
+https://msmith.de/FITfileR/articles/FITfileR.html
